815f656133fc96c8fc63c011e6d8b3371a92f8f3
JoseLuisR2 <joselu020202@gmail.com>
2026-04-19 18:19:43 -0600

    Creacion primer programa basico de estructura.

---
+++ Practica 01/Calculo_area.java
@@ -0,0 +1,13 @@
+public class Calculo_area {
+    public static void main(String[] args) {
+
+        int base = 10;
+        int altura = 5;
+
+
+        int area = base * altura;
+
+        System.out.println("Reporte de calculo");
+        System.out.println("El area calculada es: " + area);
+    }
+}
