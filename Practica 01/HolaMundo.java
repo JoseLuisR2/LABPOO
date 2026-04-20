68fd1bbf6f36badac5c3808ab09a6c688d96fa85
JoseLuisR2 <joselu020202@gmail.com>
2026-04-19 18:22:13 -0600

    Creacion segundo programa basico de estructura, Hola mundo.

---
+++ Practica 01/HolaMundo.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        Date fecha = new Date();
+
+        System.out.println("Jose Luis Reyes Rincon");
+        System.out.println("2127313");
+        System.out.println(fecha);
+    }
+}
