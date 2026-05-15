ec95c65b178c2564ba5a29bfb300b2f8947e798e
JoseLuisR2 <joselu020202@gmail.com>
2026-05-15 14:54:14 -0600

    NaveEspacial con atributos privados y métodos de acceso, NaveEspacial.java

---
+++ Practica 03/NaveEspacial.java
@@ -0,0 +1,62 @@
+public class NaveEspacial {
+    private String nombre;
+    private double nivelCombustible;
+    private int tripulantes;
+    private Motor motor;
+
+    public NaveEspacial(String nombre, int tripulantes, Motor motor) {
+        this.setNombre(nombre);
+        this.setTripulantes(tripulantes);
+        this.nivelCombustible = 100.0;
+        this.motor = motor;
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+
+        if (nombre != null && nombre.trim().length() >= 3) {
+            this.nombre = nombre;
+        } else {
+            System.out.println("Error de registro: El nombre de la nave debe ser descriptivo y mayor a 2 caracteres.");
+        }
+    }
+
+    public int getTripulantes() {
+        return tripulantes;
+    }
+
+    public void setTripulantes(int tripulantes) {
+
+        if (tripulantes >= 0 && tripulantes <= 10) {
+            this.tripulantes = tripulantes;
+        } else {
+            System.out.println("Alerta: Capacidad de tripulación fuera de rango (Permitido: 0 a 10 ocupantes).");
+        }
+    }
+
+    public double getNivelCombustible() {
+        return nivelCombustible;
+    }
+
+
+    public void setNivelCombustible(double nivel) {
+        if (nivel >= 0.0 && nivel <= 100.0) {
+            this.nivelCombustible = nivel;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return ">>> Expediente Técnico de Nave <<<\n" +
+                "Identificador: " + nombre + "\n" +
+                "Energía actual: " + nivelCombustible + "%\n" +
+                "Personal a bordo: " + tripulantes + " tripulantes\n" +
+                "Estado del Motor: " + (motor != null ? motor.toString() : "No asignado");
+    }
+}
