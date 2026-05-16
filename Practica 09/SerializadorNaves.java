a3792762f7db6d5ccecbe66bd72457d58b6b97cf
JoseLuisR2 <joselu020202@gmail.com>
2026-05-16 17:21:07 -0600

    Limpieza: eliminando archivos compilados y carpeta out

--- Practica 09/SerializadorNaves.java
+++ Practica 09/SerializadorNaves.java
@@ -1,21 +1,4 @@
-import java.io.*;
-import java.util.List;
+package PACKAGE_NAME;
 
 public class SerializadorNaves {
-    public static void guardar(List<NaveRegistro> naves, String ruta) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))) {
-            oos.writeObject(naves);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public static List<NaveRegistro> cargar(String ruta) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ruta))) {
-            return (List<NaveRegistro>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            return null;
-        }
-    }
 }
